160b0295fa167688f74aa165db7a1fe9a724ca3e
AlexTznDev <alex.tuysuzian91@gmail.com>
2026-06-02 12:48:21 +0200

    update

--- src/index.js
+++ src/index.js
@@ -5,4 +5,17 @@ import './components/slider';
 import './components/smoothScroll';
 
 window.Webflow ||= [];
-window.Webflow.push(() => {});
+window.Webflow.push(() => {
+  let lastWidth = window.innerWidth;
+  let resizeTimeout;
+  window.addEventListener('resize', () => {
+    const currentWidth = window.innerWidth;
+    if (currentWidth !== lastWidth) {
+      clearTimeout(resizeTimeout);
+      resizeTimeout = setTimeout(() => {
+        location.reload();
+      }, 500);
+    }
+    lastWidth = currentWidth;
+  });
+});
